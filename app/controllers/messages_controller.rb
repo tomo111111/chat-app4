b73be49790ff7f44b2af849928caa51067c7794f
tomo111111 <ttt0206sss@gmail.com>
2020-08-13 07:11:42 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
-    @messages = @room.messages.includes(:user)
+    @messages = @room.messages.order("created_at DESC").includes(:user)
   end
   def create
     @room = Room.find(params[:room_id])
